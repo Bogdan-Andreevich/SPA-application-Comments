05240445fa6efc31cbaf16dcd343e0f875a868eb
Bogdan <ser.butenko2010@gmail.com>
2023-03-25 00:00:51 +0200

    Added code for future responses to comments, testing different templates

--- resources/views/сascading-сomments.blade.php
+++ resources/views/сascading-сomments.blade.php
@@ -6,8 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <script defer src="{{ asset("js/CommentsController.js")}}"></script>
-    <script defer src="{{ asset("js/TimeConverter.js")}}"></script>
+    <script defer src="{{ asset("js/Comments.js")}}"></script>
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
     <link rel="stylesheet" href="{{ asset("css/CommentsController.css") }}">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
@@ -44,10 +43,79 @@
                         <label for="comment">Комментарий</label>
                         <textarea class="form-control" name="text" id="comment" rows="5" required></textarea>
                     </div>
+
+                    @if ($errors->has('comment'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('comment') }}</strong>
+                        </span>
+                    @endif
                     <button type="submit" class="btn btn-primary">Добавить комментарий</button>
                 </form>
                 <hr>
-                <div id="comments-list"></div>
+                <div id="comments-list">
+                    @foreach($comments as $comment)
+                        <div class="comment">
+                            <div class="comment-info">
+                                <strong class="comment-author">{{ $comment->name }}</strong>
+                                <span class="comment-date">{{ $comment->created_at->format('d.m.Y H:i') }}</span>
+                            </div>
+                            <div class="comment-text">{{ $comment->text }}</div>
+                            <a href="#" class="reply-link">Reply</a>
+                            <div class="reply-form">
+                                <form method="POST" action="{{ route('сomments') }}">
+                                    @csrf
+                                    <div class="form-group">
+                                        <label for="name">Имя</label>
+                                        <input type="text" name="name" class="form-control" id="name" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="email">Емаил</label>
+                                        <input type="email" name="email" class="form-control" id="email" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="comment">Комментарий</label>
+                                        <textarea class="form-control" name="text" id="comment" rows="5" required></textarea>
+                                    </div>
+                                    <input type="hidden" name="parent_id" value="{{ $comment->id }}">
+                                    <button type="submit" class="btn btn-primary">Добавить комментарий</button>
+                                </form>
+                            </div>
+                            @if(count($comment->replies))
+                                <div class="nested-comments">
+                                    @foreach($comment->replies as $reply)
+                                        <div class="comment nested-comment">
+                                            <div class="comment-info">
+                                                <strong class="comment-author">{{ $reply->name }}</strong>
+                                                <span class="comment-date">{{ $reply->created_at->format('d.m.Y H:i') }}</span>
+                                            </div>
+                                            <div class="comment-text">{{ $reply->text }}</div>
+                                            <a href="#" class="reply-link">Reply</a>
+                                            <div class="reply-form">
+                                                <form method="POST" action="{{ route('сomments') }}">
+                                                    @csrf
+                                                    <div class="form-group">
+                                                        <label for="name">Имя</label>
+                                                        <input type="text" name="name" class="form-control" id="name" required>
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label for="email">Емаил</label>
+                                                        <input type="email" name="email" class="form-control" id="email" required>
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label for="comment">Комментарий</label>
+                                                        <textarea class="form-control" name="text" id="comment" rows="5" required></textarea>
+                                                    </div>
+                                                    <input type="hidden" name="parent_id" value="{{ $reply->id }}">
+                                                    <button type="submit" class="btn btn-primary">Добавить комментарий</button>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    @endforeach
+                                </div>
+                            @endif
+                        </div>
+                    @endforeach
+                </div>
             </div>
         </div>
     </div>
