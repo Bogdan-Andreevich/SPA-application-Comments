fca4744ab0ac2d660ed1864a49d61f642b1a09ec
Bogdan <ser.butenko2010@gmail.com>
2023-03-28 01:02:45 +0300

    made the secondary comment dependency to the main comment

--- database/migrations/2023_03_21_155436_create_comments_table.php
+++ database/migrations/2023_03_21_155436_create_comments_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->text('text');
             $table->unsignedBigInteger('parent_id')->nullable();
             $table->timestamps();
